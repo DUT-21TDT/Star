f65d528f7152e54243b9a54fb86c713e0079679e
Lighttx26 <ncnhatquang26@gmail.com>
2024-08-18 21:06:59 +0700

    SSMP-23: Create User table & User entity - Changelog to create `user` table - Create user entity - Create necessary enums for user

---
+++ server/src/main/java/com/pbl/star/entities/User.java
@@ -0,0 +1,73 @@
+package com.pbl.star.entities;
+
+import com.github.f4b6a3.ulid.Ulid;
+import com.github.f4b6a3.ulid.UlidCreator;
+import com.pbl.star.enums.AccountStatus;
+import com.pbl.star.enums.Gender;
+import com.pbl.star.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class User {
+    @Id
+    @Column(name = "user_id")
+    private Ulid id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private UserRole role;
+
+    @Column(name = "register_at")
+    private Instant registerAt;
+
+    @Column(name = "status")
+    private AccountStatus status;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "gender")
+    private Gender gender;
+
+    @Column(name = "private_profile")
+    private boolean privateProfile;
+
+    public User() {
+        this.id = UlidCreator.getUlid();
+    }
+}
