9ea62078a0d57df5f02c910daa2768536d2c3c92
lightx26 <ncnhatquang26@gmail.com>
2024-09-07 21:21:56 +0700

    SSMP-84: Optimize login flow - Handle authentication error - Retrieve current user from access token

--- client/src/screens/auth/Login.tsx
+++ client/src/screens/auth/Login.tsx
@@ -1,30 +1,31 @@
-import React, { useLayoutEffect, useState } from "react";
+import React from "react";
 import imageSignup from "../../assets/images/login.webp";
 import { ConfigProvider, Divider, message, QRCode } from "antd";
 import googleIcon from "../../assets/images/devicon_google.png";
 import { Link } from "react-router-dom";
 import { LoginTheme } from "../../utils/theme";
 import "../../assets/css/login.css";
+import axios from "axios";
 
 const Login: React.FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const urlAuthLogin = `${
-    import.meta.env.VITE_BACKEND_AUTH_URL
-  }/oauth2/authorize?response_type=code&client_id=${
-    import.meta.env.VITE_CLIENT_ID
-  }&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&scope=openid`;
-  useLayoutEffect(() => {
-    const hasRedirected = sessionStorage.getItem("hasRedirected");
-    if (!hasRedirected) {
-      sessionStorage.setItem("hasRedirected", "true");
-      window.location.href = urlAuthLogin;
-    } else {
-      setIsLoading(false);
-    }
-  }, [urlAuthLogin]);
 
-  if (isLoading) {
-    return null;
+  const urlAuthLogin = `${import.meta.env.VITE_BACKEND_AUTH_URL
+    }/oauth2/authorize?response_type=code&client_id=${import.meta.env.VITE_CLIENT_ID
+    }&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&scope=openid`;
+
+  const urlGoogleLogin = `${import.meta.env.VITE_BACKEND_AUTH_URL}/oauth2/authorization/google`;
+
+  const handleGoogleLogin = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    if (sessionStorage.getItem("isGoogleLogin") === null) {
+      sessionStorage.setItem("isGoogleLogin", "true");
+      window.location.href = urlAuthLogin
+    }
+  };
+
+  if (sessionStorage.getItem("isGoogleLogin")) {
+    sessionStorage.removeItem("isGoogleLogin")
+    window.location.href = urlGoogleLogin
   }
 
   return (
@@ -42,15 +43,31 @@ const Login: React.FC = () => {
           </div>
           <div>
             <form
-              action={`${import.meta.env.VITE_BACKEND_AUTH_URL}/login`}
-              method="post"
-              encType="application/x-www-form-urlencoded"
-              onSubmit={(e) => {
+              onSubmit={async (e) => {
+                e.preventDefault(); // Ngăn chặn hành vi gửi form mặc định
                 const username = (e.target as HTMLFormElement).username.value;
                 const password = (e.target as HTMLFormElement).password.value;
                 if (username === "" || password === "") {
-                  e.preventDefault();
-                  message.error("Username and password are required.");
+                  message.error("Tên đăng nhập và mật khẩu là bắt buộc.");
+                  return;
+                }
+                try {
+                  const response = await axios.post(`${import.meta.env.VITE_BACKEND_AUTH_URL}/login`, {
+                    username,
+                    password
+                  }, {
+                    headers: {
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    withCredentials: true
+                  });
+
+                  if (response.status === 200) {
+                    window.location.href = urlAuthLogin;
+                  }
+                } catch (error) {
+                  console.error("Lỗi đăng nhập:", error);
+                  message.error("Đăng nhập thất bại, vui lòng kiểm tra lại thông tin đăng nhập");
                 }
               }}
             >
@@ -92,11 +109,7 @@ const Login: React.FC = () => {
             </p>
           </div>
           <Divider style={{ color: "#bdbdbd" }}>or</Divider>
-          <a
-            href={`${
-              import.meta.env.VITE_BACKEND_AUTH_URL
-            }/oauth2/authorization/google`}
-          >
+          <a onClick={handleGoogleLogin}>
             <div className="border border-black/15 p-[20px_25px] rounded-[15px] flex items-center justify-between gap-2 cursor-pointer">
               <div className="w-[35px] h-[35px]">
                 <img
@@ -134,58 +147,6 @@ const Login: React.FC = () => {
         </div>
       </div>
     </ConfigProvider>
-    // <div className="container">
-    //   <div className="screen">
-    //     <div className="screen__content">
-    //       <form
-    //         className="login"
-    //         action="http://localhost:8081/login"
-    //         method="post"
-    //         encType="application/x-www-form-urlencoded"
-    //       >
-    //         <div className="login__field">
-    //           <i className="login__icon fas fa-user"></i>
-    //           <input
-    //             type="text"
-    //             name="username"
-    //             className="login__input"
-    //             placeholder="Username"
-    //           />
-    //         </div>
-    //         <div className="login__field">
-    //           <i className="login__icon fas fa-lock"></i>
-    //           <input
-    //             type="password"
-    //             name="password"
-    //             className="login__input"
-    //             placeholder="Password"
-    //           />
-    //         </div>
-    //         <button className="button login__submit">
-    //           <span className="button__text">Log In Now</span>
-    //           <i className="button__icon fas fa-chevron-right"></i>
-    //         </button>
-    //       </form>
-    //       <div className="social-login">
-    //         <h3>log in via</h3>
-    //         <div className="social-icons">
-    //           <a
-    //             href="http://localhost:8081/oauth2/authorization/google"
-    //             className="social-login__icon fab fa-google"
-    //           >
-    //             Google
-    //           </a>
-    //         </div>
-    //       </div>
-    //     </div>
-    //     <div className="screen__background">
-    //       <span className="screen__background__shape screen__background__shape4"></span>
-    //       <span className="screen__background__shape screen__background__shape3"></span>
-    //       <span className="screen__background__shape screen__background__shape2"></span>
-    //       <span className="screen__background__shape screen__background__shape1"></span>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
