4c67d02805e8e168d2092d21498ba5683efa4b6a
nguyenthuchoang <nguyenthuchoang17112003@gmail.com>
2024-09-09 11:26:47 +0700

    [SSMP-87]:Add dropdown logout in admin page

--- client/src/components/layout-admin/header-admin.tsx
+++ client/src/components/layout-admin/header-admin.tsx
@@ -1,7 +1,11 @@
 import { BarsOutlined } from "@ant-design/icons";
-import { Avatar, Button, Input, Layout } from "antd";
+import { Avatar, Button, Input, Layout, Dropdown } from "antd";
 import { SearchOutlined, BellOutlined } from "@ant-design/icons";
 import react from "../../assets/images/QR.svg";
+import type { MenuProps } from "antd";
+import Cookies from "js-cookie";
+import { logOut } from "../../service/userAPI";
+
 interface IProps {
   collapsed: boolean;
   setCollapsed: (collapsed: boolean) => void;
@@ -10,6 +14,21 @@ interface IProps {
 const { Header } = Layout;
 const HeaderAdmin = (props: IProps) => {
   const { collapsed, setCollapsed } = props;
+  const handleLogout = async () => {
+    await logOut();
+    Cookies.remove("JSESSIONID");
+    Cookies.remove("id_token");
+    Cookies.remove("refresh_token");
+    Cookies.remove("access_token");
+    window.location.href = "/login";
+  };
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: <div onClick={handleLogout}>Log out</div>,
+    },
+  ];
+
   return (
     <Header style={{ padding: 0, background: "#f5f5f5" }}>
       <div className="flex justify-between items-center p-[20px] h-[64px]">
@@ -33,15 +52,21 @@ const HeaderAdmin = (props: IProps) => {
         />
         <div className="flex justify-center gap-3 flex-1">
           <Button icon={<BellOutlined />} className="border-none" />
-          <Avatar
-            shape="square"
-            size={32}
-            src={react}
-            style={{
-              cursor: "pointer",
-              border: "1px solid #f0f0f0",
-            }}
-          />
+          <Dropdown menu={{ items }} placement="bottomRight">
+            <Button
+              icon={
+                <Avatar
+                  shape="square"
+                  size={32}
+                  src={react}
+                  style={{
+                    cursor: "pointer",
+                    border: "1px solid #f0f0f0",
+                  }}
+                />
+              }
+            ></Button>
+          </Dropdown>
         </div>
       </div>
     </Header>
--- client/src/utils/customizeAxios.ts
+++ client/src/utils/customizeAxios.ts
@@ -1,11 +1,23 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 const instance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
   withCredentials: true,
 });
 
 // Add a request interceptor to set the Authorization header
-instance.interceptors.request.use();
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("access_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 // Add a request interceptor
 instance.interceptors.request.use();
