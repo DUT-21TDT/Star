aed6a3ee38d9da6bd28d2aa6bc54353ab74afd5a
lightx26 <ncnhatquang26@gmail.com>
2024-09-18 13:48:07 +0700

    SSMP-116: Patch: add client web url attribute

--- server/src/main/java/com/pbl/star/events/eventlisteners/impl/SignUpListenerImpl.java
+++ server/src/main/java/com/pbl/star/events/eventlisteners/impl/SignUpListenerImpl.java
@@ -11,6 +11,7 @@ import jakarta.mail.Multipart;
 import jakarta.mail.internet.MimeBodyPart;
 import jakarta.mail.internet.MimeMultipart;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,9 @@ public class SignUpListenerImpl implements SignUpListener {
 
     private final AuthService authService;
     private final EmailService emailService;
+
+    @Value("${client.web.url}")
+    private String clientUrl;
 
     @Override
     @Async
@@ -40,7 +44,7 @@ public class SignUpListenerImpl implements SignUpListener {
         String recipientAddress = user.getEmail();
         String subject = "Registration Confirmation";
         String confirmationUrl
-                = "http://localhost:5173/confirm-signup?token=" + token.getToken();
+                = clientUrl + "/confirm-signup?token=" + token.getToken();
 
         MimeBodyPart confirmationUrlPart = new MimeBodyPart();
         confirmationUrlPart.setText("<a href='" + confirmationUrl + "'>Confirm</a>", "UTF-8", "html");
