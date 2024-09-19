aaad7ff5a9df8331513c8144cfffa1e3d9df7705
lightx26 <ncnhatquang26@gmail.com>
2024-09-19 12:20:09 +0700

    SSMP-117: Create Post entity

---
+++ server/src/main/java/com/pbl/star/entities/Post.java
@@ -0,0 +1,41 @@
+package com.pbl.star.entities;
+
+import com.pbl.star.utils.IdGenerator;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(generator = "ulid")
+    @GenericGenerator(name = "ulid", type = IdGenerator.class)
+    @Column(name = "post_id")
+    private String id;
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Column(name = "room_id")
+    private String roomId;
+
+    @Column(name = "parent_post_id")
+    private String parentPostId;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+}
