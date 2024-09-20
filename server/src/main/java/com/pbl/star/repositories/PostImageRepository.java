b034dd591e296ac2aba1a79f75d9591b804e1a3a
lightx26 <ncnhatquang26@gmail.com>
2024-09-20 15:56:06 +0700

    SSMP-118: Business to create a new post in a room

---
+++ server/src/main/java/com/pbl/star/repositories/PostImageRepository.java
@@ -0,0 +1,7 @@
+package com.pbl.star.repositories;
+
+import com.pbl.star.entities.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, String> {
+}
