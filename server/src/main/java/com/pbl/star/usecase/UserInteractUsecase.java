64cc009c786fcb4b1cb0be636c79510dc3a49573
lightx26 <ncnhatquang26@gmail.com>
2024-09-21 14:57:59 +0700

    SSMP-122: Use userid to access to user profile instead of username

--- server/src/main/java/com/pbl/star/usecase/UserInteractUsecase.java
+++ server/src/main/java/com/pbl/star/usecase/UserInteractUsecase.java
@@ -3,5 +3,5 @@ package com.pbl.star.usecase;
 import com.pbl.star.dtos.response.user.PublicProfileResponse;
 
 public interface UserInteractUsecase {
-    PublicProfileResponse getPublicProfile(String username);
+    PublicProfileResponse getPublicProfile(String userId);
 }
