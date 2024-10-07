7358f892086e37bc020ba5809cab73131752eb76
lightx26 <ncnhatquang26@gmail.com>
2024-10-07 11:00:12 +0700

    SSMP-144: Get overview of post on user's wall

--- server/src/main/java/com/pbl/star/repositories/PostRepository.java
+++ server/src/main/java/com/pbl/star/repositories/PostRepository.java
@@ -1,9 +1,10 @@
 package com.pbl.star.repositories;
 
 import com.pbl.star.entities.Post;
+import com.pbl.star.repositories.extensions.PostRepositoryExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostRepository extends JpaRepository<Post, String> {
+public interface PostRepository extends JpaRepository<Post, String>, PostRepositoryExtension {
 }
