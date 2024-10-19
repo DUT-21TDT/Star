5653d3be9213e24427b5441fa43fe84af4579a83
lightx26 <ncnhatquang26@gmail.com>
2024-10-19 14:17:33 +0700

    SSMP-159: Optimize projection query

--- server/src/main/java/com/pbl/star/dtos/query/user/GeneralInformation.java
+++ server/src/main/java/com/pbl/star/dtos/query/user/GeneralInformation.java
@@ -2,12 +2,17 @@ package com.pbl.star.dtos.query.user;
 
 import com.pbl.star.enums.AccountStatus;
 import com.pbl.star.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-public interface GeneralInformation {
-    String getId();
-    String getUsername();
-    String getAvatarUrl();
-    UserRole getRole();
-    AccountStatus getStatus();
-
+@Getter
+@Setter
+@AllArgsConstructor
+public class GeneralInformation {
+    private String id;
+    private String username;
+    private String avatarUrl;
+    private UserRole role;
+    private AccountStatus status;
 }
