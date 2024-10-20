600300d1f7275fe18f01aab7496634089d7d105c
lightx26 <ncnhatquang26@gmail.com>
2024-10-20 13:20:53 +0700

    SSMP-166: Handle multiple object keys at once

--- server/src/main/java/com/pbl/star/services/external/impl/S3ServiceImpl.java
+++ server/src/main/java/com/pbl/star/services/external/impl/S3ServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pbl.star.services.external.impl;
 
+import com.pbl.star.dtos.response.image.GetPresignedUrlsResponse;
 import com.pbl.star.services.external.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,11 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
 
 import java.time.Duration;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +25,34 @@ public class S3ServiceImpl implements S3Service {
     private String bucketName;
 
     @Override
-    public String generatePresignedUrl(String objectKey) {
+    public GetPresignedUrlsResponse generatePresignedUrls(Set<String> objectKeys) {
+
+        Set<String> failedKeys = new HashSet<>(); // Create a list to store failed keys
+
+        Set<CompletableFuture<String>> futures = objectKeys.stream()
+                .map(objectKey -> CompletableFuture.supplyAsync(() -> generatePresignedUrlForKey(objectKey))
+                        .handle((result, ex) -> {
+                            if (ex != null) {
+                                failedKeys.add(objectKey);
+                                return null;
+                            }
+                            return result;
+                        }))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+
+        // Wait for all tasks to complete and collect the results
+        Set<String> successUrls = futures.stream()
+                .map(CompletableFuture::join) // Collect results
+                .collect(Collectors.toSet());
+
+        return GetPresignedUrlsResponse.builder()
+                .successUrls(successUrls)
+                .missingFiles(failedKeys)
+                .build();
+    }
+
+    private String generatePresignedUrlForKey(String objectKey) {
         PutObjectRequest objectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(objectKey)
--- server/src/main/java/com/pbl/star/usecase/ImageUploadUsecase.java
+++ server/src/main/java/com/pbl/star/usecase/ImageUploadUsecase.java
@@ -1,5 +1,9 @@
 package com.pbl.star.usecase;
 
+import com.pbl.star.dtos.response.image.GetPresignedUrlsResponse;
+
+import java.util.List;
+
 public interface ImageUploadUsecase {
-    String generatePresignedUrl(String fileName);
+    GetPresignedUrlsResponse generatePresignedUrls(List<String> fileNames);
 }
