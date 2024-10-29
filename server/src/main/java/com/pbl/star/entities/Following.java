59c5ce54e7375b88f7a9a448505d0f69472f3065
lightx26 <ncnhatquang26@gmail.com>
2024-10-29 14:29:29 +0700

    SSMP-172: Initialize Following entity - Create Following table, add indexes on necessary columns - Create Following entity class

---
+++ server/src/main/java/com/pbl/star/entities/Following.java
@@ -0,0 +1,37 @@
+package com.pbl.star.entities;
+
+import com.pbl.star.enums.FollowRequestStatus;
+import com.pbl.star.utils.IdGenerator;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "following")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Following {
+    @Id
+    @GeneratedValue(generator = "ulid")
+    @GenericGenerator(name = "ulid", type = IdGenerator.class)
+    @Column(name = "following_id")
+    private String id;
+
+    @Column(name = "follower_id")
+    private String followerId;
+
+    @Column(name = "followee_id")
+    private String followeeId;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private FollowRequestStatus status;
+
+    @Column(name = "follow_at")
+    private Instant followAt;
+}
