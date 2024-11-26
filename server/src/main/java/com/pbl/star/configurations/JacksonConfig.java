59d9c25ef369c6e57b526b5b08e3045e49bd5cb4
lightx26 <ncnhatquang26@gmail.com>
2024-11-26 19:28:57 +0700

    SSMP-220: Config jackson to convert object in queue

--- server/src/main/java/com/pbl/star/configurations/JacksonConfig.java
+++ server/src/main/java/com/pbl/star/configurations/JacksonConfig.java
@@ -2,14 +2,10 @@ package com.pbl.star.configurations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class JacksonConfig {
 
-    @Bean
-    public ObjectMapper objectMapper() {
+    public ObjectMapper queueObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         // Register the JavaTimeModule for handling Java 8 date/time types
         objectMapper.registerModule(new JavaTimeModule());
