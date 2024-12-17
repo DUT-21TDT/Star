f16de18f0b21740426b806a0faf4eb07b030d200
lightx26 <ncnhatquang26@gmail.com>
2024-12-17 09:18:33 +0700

    Hotfix: add GeneralInformation class constructor with User argument

--- server/src/main/java/com/pbl/star/dtos/query/user/GeneralInformation.java
+++ server/src/main/java/com/pbl/star/dtos/query/user/GeneralInformation.java
@@ -1,5 +1,6 @@
 package com.pbl.star.dtos.query.user;
 
+import com.pbl.star.entities.User;
 import com.pbl.star.enums.AccountStatus;
 import com.pbl.star.enums.UserRole;
 import lombok.Getter;
@@ -22,5 +23,14 @@ public class GeneralInformation {
         this.role = role;
         this.status = status;
         this.hasPassword = password != null;
+    }
+
+    public GeneralInformation(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.avatarUrl = user.getAvatarUrl();
+        this.role = user.getRole();
+        this.status = user.getStatus();
+        this.hasPassword = user.getPassword() != null;
     }
 }
