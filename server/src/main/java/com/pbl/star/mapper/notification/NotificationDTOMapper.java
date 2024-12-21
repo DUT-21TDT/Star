39d2c9a465f6a93df5ca30a673c72f90c60e9bc3
lightx26 <ncnhatquang26@gmail.com>
2024-12-21 21:54:10 +0700

    Hotfix: fix mapper not map boolean properties:
    - Due to Mapstruct & Lombok have different naming convention for boolean properties

--- server/src/main/java/com/pbl/star/mapper/notification/NotificationDTOMapper.java
+++ server/src/main/java/com/pbl/star/mapper/notification/NotificationDTOMapper.java
@@ -3,8 +3,10 @@ package com.pbl.star.mapper.notification;
 import com.pbl.star.dtos.response.notification.NotificationForUserResponse;
 import com.pbl.star.models.projections.notification.NotificationForUser;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class NotificationDTOMapper {
+    @Mapping(source = "read", target = "isRead")
     public abstract NotificationForUserResponse toDTO(NotificationForUser entity);
 }
--- server/src/main/java/com/pbl/star/mapper/post/PostDTOMapper.java
+++ server/src/main/java/com/pbl/star/mapper/post/PostDTOMapper.java
@@ -3,9 +3,12 @@ package com.pbl.star.mapper.post;
 import com.pbl.star.dtos.response.post.*;
 import com.pbl.star.models.projections.post.*;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class PostDTOMapper {
+    @Mapping(source = "liked", target = "isLiked")
+    @Mapping(source = "reposted", target = "isReposted")
     public abstract PostForUserResponse toDTO(PostForUser post);
     public abstract ReplyOnWallResponse toDTO(ReplyOnWall post);
     public abstract RepostOnWallResponse toDTO(RepostOnWall post);
