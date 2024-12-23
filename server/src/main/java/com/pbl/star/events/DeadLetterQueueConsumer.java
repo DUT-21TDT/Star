a33eac8832f12623ccb48965d1b6ae373056ce7e
lightx26 <ncnhatquang26@gmail.com>
2024-12-23 21:14:23 +0700

    SSMP-258: Handle dead message

--- server/src/main/java/com/pbl/star/events/DeadLetterQueueConsumer.java
+++ server/src/main/java/com/pbl/star/events/DeadLetterQueueConsumer.java
@@ -4,6 +4,7 @@ import com.pbl.star.configurations.RabbitMQConfig;
 import com.pbl.star.services.external.impl.NotificationProducerImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,7 @@ public class DeadLetterQueueConsumer {
     private static final Logger logger = LoggerFactory.getLogger(NotificationProducerImpl.class);
 
     @RabbitListener(queues = RabbitMQConfig.DLQ)
-    public void handleDeadLetter(String message) {
-        logger.error("Message in DLQ: " + message);
+    public void handleDeadLetter(Message message) {
+        logger.error("Message in DLQ: " + message.toString());
     }
 }
